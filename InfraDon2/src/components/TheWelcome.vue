df2180e4ea05a71711dbc1abec90b7ab2aada2b4
enyaschaerrer <enya.schaerrer@gmail.com>
2025-10-02 15:42:51 +0200

    init Vue

---
+++ InfraDon2/src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import {ref} from 'vue';
+const counter = ref(20);
+
+const increment = () => {
+  counter.value++;
+}
+
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <button @click="increment">Increment</button>
+  <p style="color: black; font-size: 50px;">{{ counter }}</p>
+</template>
