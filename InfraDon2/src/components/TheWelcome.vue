e04ab6361f3022015de80f49f8174a2c617630cb
enyaschaerrer <enya.schaerrer@gmail.com>
2025-10-26 12:16:56 +0100

    recup données

--- InfraDon2/src/components/TheWelcome.vue
+++ InfraDon2/src/components/TheWelcome.vue
@@ -1,16 +1,89 @@
 <script setup lang="ts">
 
-import {ref} from 'vue';
-const counter = ref(20);
+import { onMounted, ref } from 'vue';
+import PouchDB from 'pouchdb';
 
-const increment = () => {
-  counter.value++;
+declare interface Post {
+  post_name: string;
+  post_content: string;
+  attributes: {
+    creation_date: any;
+  };
 }
+
+// Référence à la base de données
+const storage = ref();
+// Données stockées
+const postsData = ref<Post[]>([])
+
+// Initialisation de la base de données
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données');
+  const db = new PouchDB('http://admin:Plkjhuio0825.@localhost:5984/db_infradon2')
+  if (db) {
+    console.log("Connecté à la collection : " + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Echec lors de la connexion à la base de données')
+  }
+}
+
+// Récupération des données
+// https://pouchdb.com/api.html#batch_fetch
+const fetchData = () => {
+  storage.value.allDocs({
+    include_docs: true
+  }).then(function (result: any) {
+    console.log('=>Données récuperées', result.rows);
+    postsData.value = result.rows.map((row: any) => row.doc);
+  }).catch(function (err: any) {
+    console.error('=> Erreur lors de la récupération des données :', err)
+  });
+}
+
+
+const addDocument = function (doc: any) {
+  storage.value
+    .post(doc)
+    .then(function (result: any) {
+      fetchData();
+    })
+    .catch(function (err: any) {
+      console.log(err);
+    })
+}
+
+const doc = {
+  post_name: "Nom de post",
+  post_content: "Contenu du post",
+  comments: [
+    {
+      title: "Hello",
+      author: "ES",
+    },
+    {
+      title: "Hi",
+      author: "ES",
+    }
+  ]
+}
+
+
+onMounted(() => {
+  console.log('=> Composant initialisé');
+  initDatabase();
+  fetchData();
+  addDocument(doc);
+});
+
 
 </script>
 
 <template>
-  <h1>Hello world</h1>
-  <button @click="increment">Increment</button>
-  <p style="color: black; font-size: 50px;">{{ counter }}</p>
+  <h1>Fetch Data</h1>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.post_name }}</h2>
+    <p>{{ post.post_content }}</p>
+  </article>
+  <button @click="addDocument(doc)">Nouveau Document</button>
 </template>
